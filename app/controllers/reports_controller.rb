7999bcfe35606df0b0f251f323f1bce666829cc8
Akash Prabhakar <akashmudliyar@gmail.com>
2015-01-12 10:43:18 +0530

    first commit

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,43 @@
+class ReportsController < ApplicationController
+	def index
+	if (params[:days])
+			@reports = Report.recentdays(params[:days]).order('datetimeperformed desc')
+		else
+			@reports = Report.all.order('datetimeperformed desc')
+		end
+	end
+
+	def show
+		@report = Report.find(params[:id])
+	end
+
+	def new
+		@report = Report.new
+	end
+
+	def create
+		@report = Report.new(params[:report].permit(:project_id, :user_id, :datetimeperformed, :hours))
+	    if @report.save
+		flash[:notice] = 'Report Created'
+		redirect_to @report
+		else
+		render 'new'
+	end
+	end
+
+def edit
+		@report = Report.find(params[:id])
+	end
+
+	def update
+		@report = Report.find(params[:id])
+
+		if @report.update(params[:report].permit(:name, :slug, :company_id, :default_rate))
+			redirect_to @report
+		else
+			render 'edit'
+		end
+	end
+
+
+end
